d8a8b2a1bf6906feffae882aa35f3491fb2ae246
LinkinM3232 <prodavecgovna47@gmail.com>
2025-09-09 21:16:47 +0300

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,5 +1,2 @@
--- В этом файле вы пишете свои SQL-запросы
--- Например:
-
--- UPDATE где SET что WHERE условие;
--- DELETE FROM где WHERE условие;
+UPDATE students SET name='Alice_Updated', age=22 WHERE id=1;
+DELETE FROM students WHERE id=2;
